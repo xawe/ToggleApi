537f14c69f2be54d3a9a9a5045f7df3b2a3b0d74
Xawe <dreamxawe@hotmail.com>
2021-05-31 10:47:38 -0300

    alteração em nome de projeto

---
+++ ToggleApi/DataAccess/IDataAccessProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToogleApi.Models;
+
+namespace ToogleApi.DataAccess
+{
+    public interface IDataAccessProvider
+    {
+        void AddFlagRecord(FeatureFlag flag);
+        void UpdateFlagRecord(FeatureFlag flag);
+        void DeleteFlagRecord(string id);
+        FeatureFlag GetFlagByIdSingleRecord(string id);
+        List<FeatureFlag> GetFlagsRecords();
+
+        FeatureFlag GetFlagByKeySingleRecord(string key);
+    }
+}
---
+++ ToggleApi/Models/FeatureFlag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToogleApi.Models
+{
+    public class FeatureFlag
+    {
+        public string Id{ get; set; }
+
+        public string Flag { get; set; }
+        public string Description { get; set; }
+        public string Value { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+    }
+}
